692e7cf9169de16c02066cbb3f44a3a848f83c6d
sanbo <sanbo.xyz@gmail.com>
2022-08-23 10:26:20 +0800

    update sth. at 2022-08-23 10:26:20

--- app/src/main/java/com/swift/sandhook/MainActivity.java
+++ app/src/main/java/com/swift/sandhook/MainActivity.java
@@ -1,11 +1,8 @@
 package com.swift.sandhook;
 
+import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,15 +10,9 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.swift.sandhook.test.Inter;
-import com.swift.sandhook.test.InterImpl;
-import com.swift.sandhook.test.PendingHookTest;
 import com.swift.sandhook.test.TestClass;
 
-import java.lang.reflect.Field;
-
-import de.robv.android.xposed.XposedHelpers;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     public static final String TAG = "SandHookTest";
 
@@ -31,18 +22,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "MainActivity onCreate");
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        TextView tv =new TextView(this);
+
+        setContentView(tv);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         StringBuilder hookTestResult = new StringBuilder();
         hookTestResult.append("当前安卓版本：").append(Build.VERSION.SDK_INT).append(":").append(Build.VERSION.PREVIEW_SDK_INT).append("\r\n");
@@ -54,16 +48,16 @@ public class MainActivity extends AppCompatActivity {
         hookTestResult.append("系统类构造方法Hook：").append(HookPass.getSystemConstructorHookResult()).append("\r\n");
         hookTestResult.append("实例方法Inline模式Hook：").append(HookPass.getInstanceMethodInlineResult()).append("\r\n");
         hookTestResult.append("实例方法Replace模式Hook：").append(HookPass.getInstanceMethodReplaceResult()).append("\r\n");
-        TextView tv = (TextView) findViewById(R.id.sample_text);
+//        TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(hookTestResult);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
 
     @Override
     protected void onPause() {
@@ -74,22 +68,6 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("dosth", hashCode() + "");
             }
         };
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            new TestClass(1).add2();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
 
