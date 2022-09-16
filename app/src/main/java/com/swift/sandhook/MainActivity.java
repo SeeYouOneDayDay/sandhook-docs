6a8b72207601cd12621d9d77810568499e633d94
sanbo <sanbo.xyz@gmail.com>
2022-09-16 12:08:19 +0800

    调整编译不过的影响项

--- app/src/main/java/com/swift/sandhook/MainActivity.java
+++ app/src/main/java/com/swift/sandhook/MainActivity.java
@@ -22,17 +22,7 @@ public class MainActivity extends Activity {
         TextView tv = new TextView(this);
 
         setContentView(tv);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+
 
         StringBuilder hookTestResult = new StringBuilder();
         hookTestResult.append("当前安卓版本：").append(Build.VERSION.SDK_INT).append(":").append(Build.VERSION.PREVIEW_SDK_INT).append("\r\n");
@@ -44,16 +34,9 @@ public class MainActivity extends Activity {
         hookTestResult.append("系统类构造方法Hook：").append(HookPass.getSystemConstructorHookResult()).append("\r\n");
         hookTestResult.append("实例方法Inline模式Hook：").append(HookPass.getInstanceMethodInlineResult()).append("\r\n");
         hookTestResult.append("实例方法Replace模式Hook：").append(HookPass.getInstanceMethodReplaceResult()).append("\r\n");
-//        TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(hookTestResult);
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
 
     @Override
     protected void onPause() {
